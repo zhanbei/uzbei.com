2846cf530675511e3adf9404f6e8fd7215b76dd1
Fisher <fisher.subs@gmail.com>
2018-07-23 17:24:45 +0800

    [.site_configs/dot-html|no-trailing-slash] Added configures for site generator.
    - to generate different versions of sites.
    - while the dot-html.js is based on no-trailing-slash.js

---
+++ .site_configs/dot-html.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const configs = require('./index');
+
+module.exports = {
+	...configs,
+	outputDir: path.resolve(__dirname, '../.site_test'),
+	mdPageTemplate: path.resolve(__dirname, 'github/dot-html-templates/page.ejs'),
+	mdListTemplate: path.resolve(__dirname, 'github/dot-html-templates/list.ejs'),
+	noTrailingSlash: false,
+};
---
+++ .site_configs/no-trailing-slash.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+	title: "Yu Zhanbei's Site",
+	inputDir: path.resolve(__dirname, '..'),
+	outputDir: path.resolve(__dirname, '../.site'),
+	mdPageTemplate: path.resolve(__dirname, 'github/no-trailing-slash-templates/page.ejs'),
+	mdPageTemplateName: 'page.ejs',
+	mdListTemplate: path.resolve(__dirname, 'github/no-trailing-slash-templates/list.ejs'),
+	mdListTemplateName: 'list.ejs',
+	assetsDir: path.resolve(__dirname, 'github/assets'),
+	// Filter out some files or folders.
+	nameFilters: [(name) => name.startsWith('_reserve'), (name) => name === 'backups'],
+	nameConverter: (name) => name.toLowerCase(),
+	// Whether to dump the
+	noTrailingSlash: true,
+	folderIndexConversion: true,
+};
