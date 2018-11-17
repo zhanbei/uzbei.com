cad2a621b41a5eb1db32d3a083ad9e7b9b948559
Yu Zhanbei <awesome.zhanbei@gmail.com>
2018-11-17 22:24:07 +0800

    [.site_configs/github] [REFACTOR] Exposed configs in three modes.

--- .site_configs/github/no-trailing-slash-templates/configs.js
+++ .site_configs/github/no-trailing-slash-templates/configs.js
@@ -1,6 +1,9 @@
 'use strict';
 
+const templates = require('../templates/configs');
+
 module.exports = {
+	...templates,
 	trailingSlash: false,
 	noTrailingSlash: true,
 };
--- .site_configs/index.js
+++ .site_configs/index.js
@@ -1,13 +1,12 @@
 'use strict';
 
 const path = require('path');
-// The selected templates, which will override the default configs.
-const templates = require('./github/templates/configs');
-// The selected mode, which will override the default configs.
-const mode = require('./github/no-trailing-slash-templates/configs');
+// The selected configs of selected theme(github), which take cares of used templates and selected mode.
+// Merge it into the site configs to make it work.
+const configures = require('./github/no-trailing-slash-templates/configs');
 
 // The default configs for the markdown site.
-const configs = {
+module.exports = Object.assign({
 	title: 'Yu Zhanbei\'s Site',
 
 	inputDir: path.resolve(__dirname, '..'),
@@ -20,15 +19,15 @@ const configs = {
 	],
 	nameConverter: (name) => name.toLowerCase(),
 
-	// Generate site in the no-trailing-slash mode, if true.
-	noTrailingSlash: false,
+	// Generate site in the dot-html mode, if true, and !trailingSlash and !noTrailingSlash.
+	dotHTML: true,
 	// Generate site in the trailing-slash mode, if true and !noTrailingSlash.
 	trailingSlash: false,
+	// Generate site in the no-trailing-slash mode, if true.
+	noTrailingSlash: false,
 
 	// The default configures used, which will be override by selected templates and mode.
 	assetsDir: path.resolve(__dirname, 'not-existed/assets'),
 	mdPageTemplate: path.resolve(__dirname, 'not-existed/templates/page.ejs'),
 	mdListTemplate: path.resolve(__dirname, 'not-existed/templates/list.ejs'),
-};
-
-module.exports = Object.assign(configs, templates, mode);
+}, configures);
